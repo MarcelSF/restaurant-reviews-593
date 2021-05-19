abf3c2e6229cccc7d082a92efba14426e1406fb7
Marcel Fonseca <marcelsf23br@gmail.com>
2021-05-19 11:01:57 +0200

    done for batch 593

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,11 @@ Rails.application.routes.draw do
     member do
       get :chef
     end
+
+    resources :reviews, only: [ :new, :create ]
   end
+
+  resources :reviews, only: [ :destroy, :show ]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
