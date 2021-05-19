b92f1ebd051b28c7ecc42d6158ff0961972327cb
Marcel Fonseca <marcelsf23br@gmail.com>
2021-05-19 09:48:45 +0200

    add review model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  resources :restaurants
+  resources :restaurants do
+    collection do
+      get :top
+      get :worst
+    end
+
+    member do
+      get :chef
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+sam = { name: "SAM", address: "Nyphemburg Strasse", stars: 5, chef_name: 'Jamie Oliver' }
+wayang =  { name: "Wayang", address: "Frankfurt", stars: 5, chef_name: 'Nigela Lawson'  }
+amarelinho =  { name: "Amarelinho", address: "Rio de Janeiro", stars: 2, chef_name: 'Yotam Ottolenghi'  }
+
+[ sam, wayang, amarelinho ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
